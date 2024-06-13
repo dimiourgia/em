db3ea8ce2086882f7046b35a249f3ea87491e60d
Navneet Parihar <pariharnavneetcs90@gmail.com>
2024-06-13 18:11:15 +0530

    dashboard order item history

--- frontend/src/Admin/components/OrdersTable.jsx
+++ frontend/src/Admin/components/OrdersTable.jsx
@@ -1,36 +1,54 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getOrders } from "../../State/AdminOrder/Action";
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from "@mui/material";
 
 const OrdersList = () => {
   const dispatch = useDispatch();
   const adminOrder = useSelector(state => state.adminOrder);
+  console.log("adminorder hai", adminOrder);
 
   useEffect(() => {
     dispatch(getOrders());
   }, [dispatch]);
 
+  const formatDate = (dateTime) => {
+    return dateTime.split('T')[0];
+  };
+
   return (
-    <div>
-      {adminOrder.orders.map(order => (
-        <div key={order._id}>
-          <h3>Order ID: {order._id}</h3>
-          <p>User: {order.user.name}</p>
-          <p>Shipping Address: {order.shippingAddress.street}</p>
-          <div>
-            <h4>Order Items:</h4>
-            {order.orderItems.map(item => (
-              <div key={item._id}>
-                <p>Product Name: {item.product.name}</p>
-                <p>Quantity: {item.quantity}</p>
-                <p>Price:{item.price}</p>
-                <p>Size: {item.size}</p>
-              </div>
-            ))}
-          </div>
-        </div>
-      ))}
-    </div>
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell>Date</TableCell>
+            <TableCell>Order ID</TableCell>
+            <TableCell>Price</TableCell>
+            <TableCell>Total Items</TableCell>
+            <TableCell>City</TableCell>
+            <TableCell>Street Name</TableCell>
+            <TableCell>Zip Code</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {adminOrder.orders.map((order) => (
+            <TableRow key={order._id}>
+              <TableCell>
+                {order.shippingAddress.firstName} {order.shippingAddress.lastName}
+              </TableCell>
+              <TableCell>{formatDate(order.shippingAddress.DateInfo)}</TableCell>
+              <TableCell>{order._id}</TableCell>
+              <TableCell>₹{order.totalDiscountedPrice}</TableCell>
+              <TableCell>{order.totalItem}</TableCell>
+              <TableCell>{order.shippingAddress.city}</TableCell>
+              <TableCell>{order.shippingAddress.streetAddress}</TableCell>
+              <TableCell>{order.shippingAddress.zipCode}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 
--- frontend/src/customer/components/Loader/Loader.jsx
+++
@@ -1,5 +0,0 @@
-import { Spinner } from "@material-tailwind/react";
-
-export default function Loader() {
-  return <Spinner className="h-16 w-16 text-gray-900/50" />;
-}
