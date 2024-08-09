374376115226f9548451ddb80cf31a362e7cac91
Ajay Singh <ajayaxes318@gmail.com>
2024-08-09 12:17:03 +0530

    now product images will be uploaded in mulitples resolutions

--- frontend/src/customer/components/FileUploader/Index.jsx
+++ frontend/src/customer/components/FileUploader/Index.jsx
@@ -10,10 +10,26 @@ const az_blob_container = import.meta.env.VITE_AZURE_BLOB_CONTAINER;
 const blob_endpoint = `https://${storage_account}.blob.core.windows.net/?${storage_sas_token}`;
 
 // Configuration options for resizing
-const imageResizeConfig = {
+const imageResizeConfig_original = {
     quality: 0.95,
     maxWidth: 2048,
     maxHeight: 2048,
+    autoRotate: true,
+    debug: true,
+};
+
+const imageResizeConfig_thumbnail = {
+    quality: 0.5,
+    maxWidth: 200,
+    maxHeight: 200,
+    autoRotate: true,
+    debug: true,
+};
+
+const imageResizeConfig_medium = {
+    quality: 0.8,
+    maxWidth: 1024,
+    maxHeight: 1024,
     autoRotate: true,
     debug: true,
 };
@@ -61,9 +77,15 @@ const FileUploader = ({updateImageUrls}) => {
             const file = selectedFiles[i];
 
             // Resize the image
-            const resizedImage = await resizeImage(file);
+            const resizedImage_hq = await resizeImage(file, 'hq');
+            const resizedImage_lq = await resizeImage(file, 'lq');
+            const resizedImage_mq = await resizeImage(file, 'mq');
 
-            const result = await uploadFileToAzure(resizedImage);
+            const result = await uploadFileToAzure(resizedImage_hq);
+
+            //upload other resolution images as well images as well
+            await uploadFileToAzure(resizedImage_lq);
+            await uploadFileToAzure(resizedImage_mq);
 
             if (result.success) {
                 uploadedFilesList.push({ name: file.name, url: result.url });
@@ -74,10 +96,24 @@ const FileUploader = ({updateImageUrls}) => {
         updateImageUrls(pre=>({...pre, imageUrl:uploadedFilesList.map(item=>item.url)}));
     };
 
-    const resizeImage = async (file) => {
+    const resizeImage = async (file, resolution) => {
         try {
-            const resizedImage = await readAndCompressImage(file, imageResizeConfig);
-            return new File([resizedImage], file.name, { type: file.type });
+            
+            switch(resolution){
+                case 'hq' : {
+                    const resizedImage = await readAndCompressImage(file, imageResizeConfig_original);
+                    return new File([resizedImage], file.name, { type: file.type });
+                }
+                case 'mq' : {
+                    const resizedImage = await readAndCompressImage(file, imageResizeConfig_medium);
+                    return new File([resizedImage], `${file.name}_mq`, { type: file.type });
+                }
+                case 'lq' : {
+                    const resizedImage = await readAndCompressImage(file, imageResizeConfig_thumbnail);
+                    return new File([resizedImage], `${file.name}_lq`, { type: file.type });
+                }
+            }
+            
         } catch (error) {
             console.error('Error resizing image:', error);
             return file; // Return the original file if resizing fails
