a15edb43ef0e105caceb84b9d89b81bfccd80c57
Ajay Singh <ajayaxes318@gmail.com>
2024-08-12 19:42:04 +0530

    changed build directory in vite.config.js, changed nginx proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,11 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  },
   resolve: {
     alias: {
         '@': path.resolve(__dirname, './src'),
