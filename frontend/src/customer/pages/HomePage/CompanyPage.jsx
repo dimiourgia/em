787fbbfdeaff30d485ee380188d40c58929a76cd
Ajay Singh <ajayaxes318@gmail.com>
2024-08-12 15:29:32 +0530

    optimized code dispatch code, commented unncessary imports

--- frontend/src/customer/pages/HomePage/CompanyPage.jsx
+++ frontend/src/customer/pages/HomePage/CompanyPage.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
-import CompanyTopBanner from '../../components/CompanyTopBanner/CompanyTopBanner'
 import CompanyTopMessage from '../../components/CompanyTopMessage/CompanyTopMessage'
-import CompanyMiddleImage from '../../components/CompanyMiddleImage/CompanyMiddleImage'
-import CompanyBottomCard from '../../components/CompanyBottomCard/CompanyBottomCard'
+
 
 
 const CompanyPage = () => {
