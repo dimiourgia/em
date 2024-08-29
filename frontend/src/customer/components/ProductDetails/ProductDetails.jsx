4bac0579cef80a010a70fd70661c3baa36b4b038
Ajay Singh <ajayaxes318@gmail.com>
2024-08-29 17:47:13 +0530

    minor changes to product details page

--- frontend/src/customer/components/ProductDetails/ProductDetails.jsx
+++ frontend/src/customer/components/ProductDetails/ProductDetails.jsx
@@ -111,7 +111,7 @@ export default function ProductDetails({setOpenAuthModal}) {
         </div>
 
 
-        <div className="border border-[1px] border-gray-100 rounded-md mx-4 mx-1 my-2 sm:my-0 px-5 py-2  sm:w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/4 bg-white">
+        <div className="mx-4 mx-1 my-2 sm:my-0 px-5 py-2  sm:w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/4 bg-white">
           <div className="text-lg lg:text-xl text-neutral-600 font-semibold mb-4">{product?.title}</div>
           {/* Options */}
           <div className="">
