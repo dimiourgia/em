97b53b0a416ecd7813729113b70c7c0c8db7463f
Ajay Singh <ajayaxes318@gmail.com>
2026-04-29 12:51:58 +0530

    Add comment for redeployment in CollectionProducts
    Added a comment for redeployment purposes.

--- frontend/src/customer/pages/CollectionProducts.jsx
+++ frontend/src/customer/pages/CollectionProducts.jsx
@@ -3,6 +3,8 @@ import Card from "../components/Collections/Card"
 import { useParams } from "react-router-dom";
 import Product from "../components/Product/Product";
 
+//commenting just to redeploy
+
 
 export default function(){
     const params = useParams();
